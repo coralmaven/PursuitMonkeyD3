7eade93a475dd6cbad10d903381d5fc8eee6cc77
camfoy <chfoy4@gmail.com>
2019-05-29 19:01:08 -0500

    everything

--- app.py
+++ app.py
@@ -12,31 +12,15 @@ from flask import (
 #################################################
 app = Flask(__name__)
 
-# # The database URI
-# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/pursuitmonkey.sqlite"
+# Login to Google. 
+# Only need to run this once,
+# The rest of requests will use the same session.
 
-# db = SQLAlchemy(app)
-
-
-# # Create our database model
-# class PursuitMonkey(db.Model):
-#     __tablename__ = 'emoji'
-
-#     id = db.Column(   # Login to Google. 
-    # Only need to run this once,
-    #  the rest of requests will use the same session.
 pytrend = TrendReq()
 
 #################################################
 # Flask Routes
 #################################################
-
-# Create database tables
-# @app.before_first_request
-# def setup():
-     # Recreate database each time for demo
-    # db.drop_all()
-    # db.create_all()
 
 @app.route("/")
 def home():
@@ -50,49 +34,33 @@ def interest_over_time_data(inputValue):
 
     print(f"The input is:{inputValue}")
 
-    # Create payload and capture API tokens. 
-    # Only needed for interest_over_time(), 
-    # interest_by_region() & related_queries()
-
     keywords = inputValue
 
     keywords = keywords.split(sep=',')
 
     pytrend.build_payload(kw_list=keywords)
 
-    # # Interest Over Time
+    # Interest over time
     interest_over_time_df = pytrend.interest_over_time()
+
+    # Interest by region
+    interest_by_region_df = pytrend.interest_by_region()
+
+    interest_by_region_df.reset_index(inplace=True)
 
     interest_over_time_df.reset_index(inplace=True)
 
     print(interest_over_time_df.head())
 
+    interest_by_region_df = interest_by_region_df.to_dict()
+
     interest_over_time_df = interest_over_time_df.to_dict()
 
-    return jsonify(interest_over_time_df)
+    return jsonify({
+        "interest_over_time": interest_over_time_df,
+        "interest_by_region": interest_by_region_df
+    })
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-# # Interest by Region
-# interest_by_region_df = pytrend.interest_by_region()
-# print(interest_by_region_df.head())
-
-# # Related Queries, returns a dictionary of dataframes
-# related_queries_dict = pytrend.related_queries()
-# print(related_queries_dict)
-
-# # Get Google Hot Trends data
-# trending_searches_df = pytrend.trending_searches()
-# print(trending_searches_df.head())
-
-# # Get Google Top Charts
-# top_charts_df = pytrend.top_charts(cid='actors', date=201611)
-# print(top_charts_df.head())
-
-# # Get Google Keyword Suggestions
-# suggestions_dict = pytrend.suggestions(keyword='pizza')
-# print(suggestions_dict)
--- static/js/app.js
+++ static/js/app.js
@@ -14,20 +14,13 @@ submit.on("click", function() {
 
     console.log(inputValue);
 
-    // Set the span tag in the h1 element to the text
-    // that was entered in the form
-    d3.select("small>span").text(inputValue);
     interest_over_time_data = interest_over_time(inputValue)
 })
     
 async function interest_over_time(inputValue){ 
 
     const interest_over_time_url = `/interest_over_time/${inputValue}`
-    console.log(interest_over_time_url)
-    var keyword_string = inputValue.replace('/,/g','-')
-    console.log('keyword',keyword_string);
     let interest_over_time_data = await d3.json(interest_over_time_url);
     console.log('return from py:',interest_over_time_data);
     return(interest_over_time_data)
 }
-
