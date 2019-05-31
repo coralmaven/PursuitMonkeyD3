4111f7f56fc6dff971d81cca8f2b73d88b8fafc2
camfoy <chfoy4@gmail.com>
2019-05-30 19:39:34 -0500

    update

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import os
+
 from pytrends.request import TrendReq
+
 import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
 from flask_sqlalchemy import SQLAlchemy
 from flask import (
     Flask,
     render_template,
     jsonify)
-
 
 #################################################
 # Flask Setup
@@ -19,6 +28,23 @@ app = Flask(__name__)
 pytrend = TrendReq()
 
 #################################################
+# Database Setup
+#################################################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///static/db/top_trends.db"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+initRegion = Base.classes.region
+initTime = Base.classes.time
+
+#################################################
 # Flask Routes
 #################################################
 
@@ -28,11 +54,29 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/interest_over_time/<inputValue>")
-def interest_over_time_data(inputValue):
-    """Return interest_over_time data for the keywords"""
+@app.route("/init")
+def init():
+    # Use Pandas to perform the sql query
+    time_stmt = db.session.query(initTime).statement
+    time_df = pd.read_sql_query(time_stmt, db.session.bind)
+    time_data = {
+        "date": time_df.date.values.tolist(),
+        "taco": time_df.tacos.values.tolist(),
+        "sandwich": time_df.sandwiches.values.tolist(),
+        "kebab": time_df.kebabs.values.tolist()
+    }
 
-    print(f"The input is:{inputValue}")
+    # region_stmt = db.session.query(initRegion).statement
+    # region_df = pd.read_sql_query(region_stmt, db.session.bind)
+    # region_dict = region_df.to_dict()
+
+    # Results
+    return jsonify(time_data)
+
+
+@app.route("/live_trends/<inputValue>")
+def interest_over_time_data(inputValue):
+    """Return live_trends data for the keywords"""
 
     keywords = inputValue
 
@@ -46,21 +90,19 @@ def interest_over_time_data(inputValue):
     # Interest by region
     interest_by_region_df = pytrend.interest_by_region()
 
+    # Fix dataframes
     interest_by_region_df.reset_index(inplace=True)
-
     interest_over_time_df.reset_index(inplace=True)
 
-    print(interest_over_time_df.head())
-
+    # DataFrame to Dictionary
     interest_by_region_df = interest_by_region_df.to_dict()
-
     interest_over_time_df = interest_over_time_df.to_dict()
 
+    # Results
     return jsonify({
         "interest_over_time": interest_over_time_df,
         "interest_by_region": interest_by_region_df
     })
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -14,13 +14,157 @@ submit.on("click", function() {
 
     console.log(inputValue);
 
-    interest_over_time_data = interest_over_time(inputValue)
+    live_trends_data = live_trends(inputValue)
 })
     
-async function interest_over_time(inputValue){ 
+async function live_trends(inputValue){ 
 
-    const interest_over_time_url = `/interest_over_time/${inputValue}`
-    let interest_over_time_data = await d3.json(interest_over_time_url);
-    console.log('return from py:',interest_over_time_data);
-    return(interest_over_time_data)
+    // Retrieve data
+    const live_trends_url = `/live_trends/${inputValue}`
+    const live_trends_data = await d3.json(live_trends_url);
+
+    // Print data
+    console.log('return from py:',live_trends_data);
+
+    return(live_trends_data)
 }
+
+(async function(){
+
+    // Define SVG area dimensions
+    const
+        svgWidth = 960,
+        svgHeight = 500;
+
+    // Define the chart's margins as an object
+    const margin = {
+        top: 60,
+        right: 60,
+        bottom: 60,
+        left: 60
+    };
+
+    // Define dimensions of the chart area
+    const chartWidth = svgWidth - margin.left - margin.right;
+    const chartHeight = svgHeight - margin.top - margin.bottom;
+
+    // Select body, append SVG area to it, and set its dimensions
+    const svg = d3.select("#time")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+    // Append a group area, then set its margins
+    const chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Retrieve data
+    const time_data_url = '/init'
+    const time_data = await d3.json(time_data_url);
+    console.log(time_data.date);
+
+    // Create a function to parse date and time
+    const parseTime = d3.timeParse("%Y-%m-%d");
+
+    // Format date data
+    time_data.date.forEach(function(data) {
+        data = parseTime(data);
+        console.log(data);
+    });
+    console.log(time_data.date);
+
+    // // Reformat data
+    // Object.keys(time_data).forEach(function (key) {
+    //     time_data[key] = Object.values(time_data[key])
+    // });
+
+    // // Cast values to numbers
+    // time_data.forEach(function(data) {
+        
+    //     data.tacos = +data.tacos;
+    //     data.sandwiches = +data.sandwiches;
+    //     data.kebabs = +data.kebabs;
+        
+    // });
+
+    // // Create the scales
+    // const xTimeScale = d3.scaleTime()
+    //     .domain(d3.extent(time_data.date))
+    //     .range([0, chartWidth]);
+
+    // const yLinearScale = d3.scaleLinear()
+    //     .range([chartHeight, 0]);
+
+    // Configure a line function which will plot the x and y coordinates using our scales
+    // const drawLine = d3.line()
+    //     .x(data => xTimeScale(time_data.date))
+    //     .y(data => yLinearScale(time_data.taco));
+
+    // // Find max of data
+    // const tacoMax = d3.max(time_data.taco);
+
+    // const sandwichMax = d3.max(time_data.sandwich);
+
+    // const kebabMax = d3.max(time_data.kebab);;
+
+    // let yMax;
+    // if ((tacoMax > sandwichMax) && (tacoMax > kebabMax)) {
+    //     yMax = tacoMax;
+    // }
+    // else if ((sandwichMax > tacoMax) && (sandwichMax > kebabMax)) {
+    //     yMax = sandwichMax;
+    // }
+    // else {
+    //     yMax = kebabMax;
+    // }
+
+    // // Use the yMax value to set the yLinearScale domain
+    // yLinearScale.domain([0, yMax]);
+
+    // // Create the axes
+    // const bottomAxis = d3.axisBottom(xTimeScale).tickFormat(d3.timeFormat("%Y-%m-%d"));
+    // const leftAxis = d3.axisLeft(yLinearScale);
+
+    // // Add x-axis
+    // chartGroup.append("g")
+    //     .attr("transform", `translate(0, ${chartHeight})`)
+    //     .call(bottomAxis);
+
+    // // Add y-axis
+    // chartGroup.append("g").call(leftAxis);
+
+//     // Line generator for taco data
+//     const tacoLine = d3.line()
+//         .x(d => xTimeScale(d.date))
+//         .y(d => yLinearScale(d.tacos));
+
+//     // Line generator for sandwich data
+//     const sandwichLine = d3.line()
+//         .x(d => xTimeScale(d.date))
+//         .y(d => yLinearScale(d.sandwiches));
+
+//     // Line generator for kebab data
+//     const kebabLine = d3.line()
+//         .x(d => xTimeScale(d.date))
+//         .y(d => yLinearScale(d.kebabs));
+
+//     // Append a path for tacoLine
+//     chartGroup
+//         .append("path")
+//         .attr("d", tacoLine(time_data))
+//         .classed("line green", true);
+
+//     // Append a path for sandwhichLine
+//     chartGroup
+//         .data([time_data])
+//         .append("path")
+//         .attr("d", sandwichLine)
+//         .classed("line orange", true);
+
+//     // Append a path for sandwhichLine
+//     chartGroup
+//         .data([time_data])
+//         .append("path")
+//         .attr("d", kebabLine)
+//         .classed("red", true);
+})()
